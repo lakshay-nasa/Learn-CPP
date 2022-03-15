70a81e281782f1a9c48c6e0d72446b84f3e28c9e
lakshay-nasa <lakshay1717@gmail.com>
2022-03-15 23:03:34 +0530

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hey, Nice To Meet You!";
+    return 0;
+}
+
+/* 
+#include <iostream>
+
+int main(){
+    std::cout<<"Hey, Nice To Meet You!";
+    return 0;
+}
+*/
