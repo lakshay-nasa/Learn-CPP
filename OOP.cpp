e7a3fc87040d6980983a256df3e8285aed8f1ef5
Lakshay Nasa <76848292+lakshay-nasa@users.noreply.github.com>
2022-03-30 22:40:51 +0530

    Feat: 🚀 Added OOP

---
+++ OOP.cpp
@@ -0,0 +1,14 @@
+/* Object-Oriented Programming
+
+----> Classes and objects are the two main aspects of object-oriented programming.
+
+1. Classes
+        i. Private
+        ii. Public
+        iii. Protected
+        
+
+2. Objects
+
+*/
+
